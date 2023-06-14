1bc561e497a83b22af3c8e63e3bd6639e65952e4
doranmartin <doranmartin1994@gmail.com>
2023-06-14 16:11:15 -0400

    Completed 1.3

---
+++ Chapter1/1_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+  int x;
+  double width;
+  
+  x = 7;
+  width = 20.5;
+  
+  std::cout << width * x;
+
+  /*
+  // This is best practice
+  int a;
+  int b;
+
+  // Is the same as this
+  int a, b; // not best practice
+  */
+  
+}
