fb5bb8e583e9621e8a6d741c1c554c7941aef669
doranmartin <doranmartin1994@gmail.com>
2023-06-19 15:44:45 -0400

    finished 1.4

--- Chapter1/1_4.cpp
+++ Chapter1/1_4.cpp
@@ -16,10 +16,31 @@ int main()
 
   // List initialization methods (C++11) (preferred)
   int d { 7 };    // direct list initialization
+  int d{7};       // or this way
   int e = { 8 };  // copy list initialization
   int f {};       // value initialization
 
+  int x();    // forward declaration of function x
+  int x(0);   // definition of variable x with initializer 0
+
+  int width {4.5}; // error: a number with a fractional value can't fit into an int
+
+  // Best practice - Favor initialization using braces
+
+  int width {}; // value initialization
+
+  // When to use {0} vs. {}
+  int x {0};      // explicit initialization to value 0
+  std::cout << x; // we're using that zero value
+
+  int x {};       // value initialization
+  std::cin >> x;  // we're immediately replacing that value
+
+  // maybe_unused attribute:
+  [[maybe_unused]] int x {5}; // since x is [[maybe_unused]], no warning generated
+
   
+
 
 
   return 0;
