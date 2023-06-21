d315503b9e0578a6ee905db481c635405816b411
doranmartin <doranmartin1994@gmail.com>
2023-06-21 16:37:57 -0400

    Still on 1.7

---
+++ Chapter1/1_7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+  int value; // correct
+
+  int my_variable_name; // correct (separated by underscores/snake_case)
+  int my_function_name(); // correct (separated by underscores/snake_case)
+
+  int myVariableName; // correct (intercapped/CamelCase)
+  int myFunctionName(); // correct (intercapped/CamelCase)
+
+  int my variable name; // invalid (whitespace not allowed)
+  int my function name(); // invalid (whitespace not allowed)
+
+  int MyVariableName; // valid but incorrect (should start with lower case letter)
+  int MyFunctionName(); // valid but incorrect (should start with lower case letter)
+  
+}
