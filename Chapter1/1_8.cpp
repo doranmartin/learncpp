a6bb58236203a93ca4c210c283dcae5b679aa9fe
doranmartin <doranmartin1994@gmail.com>
2023-06-22 15:23:59 -0400

    finished 1.8

---
+++ Chapter1/1_8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+  // whitespace doesn't matter
+
+  // newlines are not allowed in quoted text
+
+  /*
+  std::cout << 3 + 4
+    + 5 + 6
+    * 7 * 8;
+
+
+
+  */
+
+  
+
+
+}
